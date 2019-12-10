061ac2500d9888fe6f18d9b74d51348afc9882a4
dnievas <nievas.david@gmail.com>
2019-12-10 16:00:44 -0300

    Guardias. Exportacion de los datos de una guardia al formato csv

--- modules/guardias/controller/guardia.ts
+++ modules/guardias/controller/guardia.ts
@@ -2,12 +2,17 @@ import BaseController from "../../../core/app/basecontroller";
 import { Guardia } from "../schemas/guardia";
 import { Types } from "mongoose";
 
+const fs = require('fs');
+const csv = require('fast-csv');
+
 class GuardiaController extends BaseController {
 
     constructor(model) {
         super(model);
         this.addAndConfirmar = this.addAndConfirmar.bind(this);
         this.updateAndConfirmar = this.updateAndConfirmar.bind(this);
+        this.updateAndValidar = this.updateAndValidar.bind(this);
+        this.generarCSV = this.generarCSV.bind(this);
     }
 
     // Posibles estados de la guardia (planilla)
@@ -30,6 +35,48 @@ class GuardiaController extends BaseController {
 
     async updateAndConfirmar(req, res, next){
         return await this.saveUpdate(req, res, next, this.ESTADO_CONFIRMADA);
+    }
+
+    async updateAndValidar(req, res, next){
+        return await this.saveUpdate(req, res, next, this.ESTADO_VALIDADA);
+    }
+
+    
+    /**
+     * Genera un archivo en formato csv a partir del id de una 
+     * guardia enviado por parametro en la url.
+     * TODO Consultar las columnas y formato final para el csv  
+     */
+    async generarCSV(req, res, next){
+        try {
+            const id = req.params.id;
+            if (!id || (id && !Types.ObjectId.isValid(id))) return res.status(404).send();
+            let objToExport:any = await Guardia.findById(id).lean();
+            if (!objToExport) return res.status(404).send();
+            let ws = fs.createWriteStream('/tmp/guardia.csv', { encoding: 'utf8' });
+            
+            csv.write(objToExport.planilla, {
+                    headers: true, transform: (row) => {
+                        return {
+                            Agente: row.agente.nombre
+                        };
+                    }
+                })
+                .pipe(ws)
+                .on('finish', () => {
+                    res.download(('/tmp/guardia.csv' as string), (err) => {
+                        if (err) {
+                            next(err);
+                        } else {
+                            next();
+                        }
+                    });
+                });
+        } catch (err) {
+            return next(err);
+        }
+
+
     }
 
 
--- modules/guardias/routes/guardia.ts
+++ modules/guardias/routes/guardia.ts
@@ -9,11 +9,13 @@ export const Routes = express.Router();
 
 Routes.get(`${baseUrl}`, controller.get);
 Routes.get(`${baseUrl}/:id`, controller.getById);
+Routes.get(`${baseUrl}/:id/generar-csv`, controller.generarCSV);
 
 Routes.post(`${baseUrl}`, controller.add);
 Routes.post(`${baseUrl}/confirmar`, controller.addAndConfirmar);
 
 Routes.put(`${baseUrl}/:id`, controller.update);
 Routes.put(`${baseUrl}/:id/confirmar`, controller.updateAndConfirmar);
+Routes.put(`${baseUrl}/:id/validar`, controller.updateAndValidar);
 
 Routes.delete(`${baseUrl}/:id`, controller.delete);
