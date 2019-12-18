c830edd2a7052dfc1ef1aa45de2d8884d3d4e73c
dnievas <nievas.david@gmail.com>
2019-12-18 13:31:45 -0300

    Guardias. Generacion csv para importar a RRHH Provincia

--- modules/guardias/controller/guardia.ts
+++ modules/guardias/controller/guardia.ts
@@ -2,6 +2,8 @@ import BaseController from "../../../core/app/basecontroller";
 import { Guardia } from "../schemas/guardia";
 import { Types } from "mongoose";
 
+import * as moment from 'moment';
+
 const fs = require('fs');
 const csv = require('fast-csv');
 
@@ -45,23 +47,69 @@ class GuardiaController extends BaseController {
     /**
      * Genera un archivo en formato csv a partir del id de una 
      * guardia enviado por parametro en la url.
-     * TODO Consultar las columnas y formato final para el csv  
      */
     async generarCSV(req, res, next){
         try {
             const id = req.params.id;
             if (!id || (id && !Types.ObjectId.isValid(id))) return res.status(404).send();
-            let objToExport:any = await Guardia.findById(id).lean();
-            if (!objToExport) return res.status(404).send();
-            let ws = fs.createWriteStream('/tmp/guardia.csv', { encoding: 'utf8' });
+            let guardia:any = await Guardia.findById(id);
+            if (!guardia) return res.status(404).send();
+            let ws = fs.createWriteStream('/tmp/guardia.csv');
+
+            // Encabezado
+            const tipoGuardia = (guardia.tipoGuardia == 'pasiva')? "PASIVA":"ACTIVA";
+            const esProfesional = (guardia.categoria.nombre == 'Profesional')? "x":"TECNICO";
+            const esTecnico = (guardia.categoria.nombre == 'Técnico')? "x": (guardia.categoria.nombre == 'Auxiliar')? "AUXILIARES":"TECNICO";
+            const esAuxiliar = (guardia.categoria.nombre == 'Auxiliar')? "x":"AUXILIARES";
+            const fechaDesde = moment(guardia.periodo.fechaDesde).format('DD/MM/YYYY');
+            const fechaHasta = moment(guardia.periodo.fechaHasta).format('DD/MM/YYYY');
+            let row1 = `CERTIFICACION DE GUARDIAS ${tipoGuardia}S,,,,PROFESIONAL,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,`;
+            let row2 = `Mes:,,,,${esProfesional},,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,`;
+            let row3 = `Dependencia: HOSPITAL DR. EDUARDO CASTRO RENDON,,,,${esTecnico},,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,`;
+            let row4 = `Guardias ${tipoGuardia.toLowerCase()}s: ${fechaDesde} al ${fechaHasta},,,,${esAuxiliar},,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,`;
+            let row5 = `,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,`;
+            let filasCSV = [];
+            filasCSV.push(row1.split(','));
+            filasCSV.push(row2.split(','));
+            filasCSV.push(row3.split(','));
+            filasCSV.push(row4.split(','));
+            filasCSV.push(row5.split(','));
             
-            csv.write(objToExport.planilla, {
-                    headers: true, transform: (row) => {
-                        return {
-                            Agente: row.agente.nombre
-                        };
+            // Guardias por Agente
+            let headers = ["DEM","Servicio","Legajo","Agente","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","Total"];
+            filasCSV.push(headers);
+            let range = guardia.periodo.range();
+            guardia.planilla.forEach(guardiaAgente => {
+                let grillaGuardias = [];
+                let totalGuardias = 0;
+                grillaGuardias.push('Nro de Lote'); // TODO Guardar Lote (Crear Schema)
+                grillaGuardias.push(guardia.servicio.nombre);
+                grillaGuardias.push(guardiaAgente.agente.id); // TODO Guardar Legajo
+                grillaGuardias.push(guardiaAgente.agente.apellido);
+                range.forEach((dia, index) => {
+                    if (guardiaAgente.diasGuardia[index]){
+                        const diaCompleto = guardiaAgente.diasGuardia[index].diaCompleto;
+                        totalGuardias += diaCompleto? 1:0.5;
+                        grillaGuardias.push(diaCompleto?'X':'M');
                     }
-                })
+                    else{
+                        grillaGuardias.push('');
+                    }
+                });
+                grillaGuardias.push(totalGuardias);
+                filasCSV.push(grillaGuardias)
+            });
+
+            let footer = `Firma y Sello del Director,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,`;
+            filasCSV.push(footer.split(','));
+            csv.write(filasCSV, {
+                    headers: false
+                    // transform: (row) => {
+                    //     return {
+                    //         Agente: row.agente.nombre
+                    //     };
+                    // }
+                })   
                 .pipe(ws)
                 .on('finish', () => {
                     res.download(('/tmp/guardia.csv' as string), (err) => {
--- modules/guardias/schemas/guardiaperiodo.ts
+++ modules/guardias/schemas/guardiaperiodo.ts
@@ -6,5 +6,28 @@ export const GuardiaPeriodoSchema = new Schema({
     nombre: String
 })
 
+GuardiaPeriodoSchema.methods.range = function(cb) {
+    let _fechaDesde = new Date(this.fechaDesde);
+    let range = [];
+    if (_fechaDesde && this.fechaHasta){
+        while (_fechaDesde.getTime() <= this.fechaHasta.getTime()) {
+            const tomorrow = addOneDay(_fechaDesde);
+            if ( tomorrow.getMonth() != _fechaDesde.getMonth()){
+                let dayNumber = _fechaDesde.getDate();
+                while (dayNumber < 31){
+                    range.push(null);
+                    dayNumber +=1;
+                }
+            }
+            range.push(_fechaDesde);
+            _fechaDesde = tomorrow;
+        }
+    }
+    return range;  };
+
+function addOneDay(date:Date){
+    let tomorrow = new Date(date);
+    return new Date(tomorrow.setDate(tomorrow.getDate() + 1 ));
+}
 
 export const GuardiaPeriodo = model('GuardiaPeriodo', GuardiaPeriodoSchema, 'guardiaperiodos');
