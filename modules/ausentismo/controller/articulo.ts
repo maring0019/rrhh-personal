43ef792a9751c7c43cd6472cc1256a55d79fadf5
dnievas <nievas.david@gmail.com>
2020-03-11 13:55:23 -0300

    Articulos. Ajuste metodo de actualizacion para replicar cambios en el color en otros modelos que lo referencian

--- modules/ausentismo/controller/articulo.ts
+++ modules/ausentismo/controller/articulo.ts
@@ -1,6 +1,34 @@
 import BaseController from '../../../core/app/basecontroller';
+import { Types } from 'mongoose';
+
+import { Articulo } from '../schemas/articulo';
+import { AusenciaPeriodo } from '../schemas/ausenciaperiodo';
 
 class ArticuloController extends BaseController {
+
+    /**
+     * Actualiza los datos de un articulo existente, y posteriormente
+     * se intenta actualizar en forma masiva todas las referencias al
+     * color del articulo en los objetos AusenciaPeriodo para que se
+     * visualicen correctamente por ejemplo en el calendario.
+     */
+    async update(req, res, next) {
+        try {
+            const id = req.params.id;
+            if (!id || (id && !Types.ObjectId.isValid(id))) return res.status(404).send();
+            let objToUpdate:any = await Articulo.findById(id);
+            if (!objToUpdate) return res.status(404).send({ message:"Not found"});
+            let objWithChanges = req.body;
+            await objToUpdate.updateOne({ $set: objWithChanges });
+            await AusenciaPeriodo.updateMany(
+                { 'articulo._id' : Types.ObjectId(objToUpdate._id) },
+                { $set: { 'articulo.color' : objWithChanges.color } } 
+            )
+            return res.json(objToUpdate);
+        } catch (err) {
+            return next(err);
+        }
+    }
 
 }
 
--- modules/ausentismo/schemas/articulo.ts
+++ modules/ausentismo/schemas/articulo.ts
@@ -3,24 +3,33 @@ import { FormulaSchema } from './formula';
 
 export const ArticuloSchema = new Schema({
     
-    idInterno: {                  // Codigo Interno Sistema Anterior. TODO Remover si no es mas necesario
+    idInterno: {                // Codigo Interno Sistema Anterior. TODO Remover si no es mas necesario
         type: Number
     },
-    codigo: {                     // Codigo alfanumerico
+    codigo: {                   // Codigo alfanumerico
         type: String,
         required: true
     },
-    nombre: {                     // Nombre del articulo
+    nombre: {                   // Nombre del articulo
         type: String,
         required: true
     },
     descripcion: String,
-    color: String,                 // Verde claro=#5cb85c, rojo= #d9534f
-    diasCorridos: Boolean,         // Indica si los dias a ausencia son corridos
-    diasHabiles: Boolean,          // Indica si los dias a ausencia son habiles
-    descuentaDiasLicencia: Boolean,// Para uso de articulos especiales que descuentan dias de licencia
-    formulas: [FormulaSchema],     // Conjunto de restricciones/validaciones a aplicar al momento de
-                                   // crear o actualizar un ausentismo.   
+    color: String,              // A visualizar cuando sea necesario. Verde claro=#5cb85c, rojo= #d9534f
+    diasCorridos: {             // Indica si los dias a ausencia son corridos
+        type: Boolean,
+        default: true
+    },         
+    diasHabiles: {              // Indica si los dias a ausencia son habiles
+        type: Boolean,
+        default: false
+    },        
+    descuentaDiasLicencia: {    // Para uso de articulos especiales que descuentan dias de licencia
+        type: Boolean,
+        default: false
+    },
+    formulas: [FormulaSchema],  // Conjunto de restricciones/validaciones a aplicar al momento de
+                                // crear o actualizar un ausentismo.   
     
     // Los siguientes atributos se mantienen como parte legacy del sistema
     // anterior, pero no son utilizados actualmente. 
