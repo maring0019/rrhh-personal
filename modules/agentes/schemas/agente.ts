e22d70b60f6585bb61eabb63f6dc60e5be5e0e3a
David Nievas <nievas.david@gmail.com>
2020-09-08 16:14:46 -0300

    Agente. Fix instance method para recuperar correctamente los servicios (ahora ubicaciones) que tiene acceso el agente cuando es jefe de servicio

--- modules/agentes/schemas/agente.ts
+++ modules/agentes/schemas/agente.ts
@@ -1,96 +1,97 @@
-import { Schema, model } from 'mongoose';
-const audit = require('../../../packages/mongoose-audit-trail');
+import { Schema, model } from "mongoose";
+const audit = require("../../../packages/mongoose-audit-trail");
 
-import { parseDate } from '../../../core/utils/dates';
-import { constantes } from '../../../core/constants/index';
-import { DireccionSchema } from '../../../core/tm/schemas/direccion';
-import { PaisSchema } from '../../../core/tm/schemas/pais';
-import { ContactoSchema } from '../../../core/tm/schemas/contacto';
-import { EducacionSchema } from '../../../core/tm/schemas/educacion';
-import { EspecialidadSchema } from '../../../core/tm/schemas/especialidad';
-import { SituacionLaboralSchema } from './situacionlaboral';
-import { HistoriaLaboralSchema } from './historialaboral';
+import { parseDate } from "../../../core/utils/dates";
+import { constantes } from "../../../core/constants/index";
+import { DireccionSchema } from "../../../core/tm/schemas/direccion";
+import { PaisSchema } from "../../../core/tm/schemas/pais";
+import { ContactoSchema } from "../../../core/tm/schemas/contacto";
+import { EducacionSchema } from "../../../core/tm/schemas/educacion";
+import { EspecialidadSchema } from "../../../core/tm/schemas/especialidad";
+import { SituacionLaboralSchema } from "./situacionlaboral";
+import { HistoriaLaboralSchema } from "./historialaboral";
 
 export const AgenteSchema = new Schema({
-    idLegacy: Number,      // ID Sistema anterior.
-    numero: String,        // En el alta aun no esta disponible este dato
-    tipoDocumento: String, // No deberia utilizarse mas. Solo DU
-    documento: {
-        type: String,
-        required: true,
-        es_indexed: true
-    },
-    foto: String, // Encode64
-    cuil: {
-        type: String,
-        required: false,   // No todos los agentes tienen CUIL en SQLServer
-        es_indexed: true
-    },
-    nombre: {
-        type: String,
-        required: true,
-        es_indexed: true
-    },
-    apellido: { 
-        type: String,
-        required: true,
-        es_indexed: true
-    },
-    estadoCivil: constantes.ESTADOCIVIL,
-    sexo: constantes.SEXO,
-    genero: constantes.GENERO,
-    fechaNacimiento: {
-        type: Date,
-        set: parseDate,
-        get: parseDate,
-        es_indexed: true
-    },
-    nacionalidad: PaisSchema,
-    direccion: DireccionSchema,
-    contactos: [ContactoSchema],
-    educacion: [EducacionSchema],
-    especialidad: EspecialidadSchema, // TODO Ver especialidadSchema
-    situacionLaboral: SituacionLaboralSchema,
-    historiaLaboral: [HistoriaLaboralSchema],
-    activo: Boolean,
-    
+	idLegacy: Number, // ID Sistema anterior.
+	numero: String, // En el alta aun no esta disponible este dato
+	tipoDocumento: String, // No deberia utilizarse mas. Solo DU
+	documento: {
+		type: String,
+		required: true,
+		es_indexed: true,
+	},
+	foto: String, // Encode64
+	cuil: {
+		type: String,
+		required: false, // No todos los agentes tienen CUIL en SQLServer
+		es_indexed: true,
+	},
+	nombre: {
+		type: String,
+		required: true,
+		es_indexed: true,
+	},
+	apellido: {
+		type: String,
+		required: true,
+		es_indexed: true,
+	},
+	estadoCivil: constantes.ESTADOCIVIL,
+	sexo: constantes.SEXO,
+	genero: constantes.GENERO,
+	fechaNacimiento: {
+		type: Date,
+		set: parseDate,
+		get: parseDate,
+		es_indexed: true,
+	},
+	nacionalidad: PaisSchema,
+	direccion: DireccionSchema,
+	contactos: [ContactoSchema],
+	educacion: [EducacionSchema],
+	especialidad: EspecialidadSchema, // TODO Ver especialidadSchema
+	situacionLaboral: SituacionLaboralSchema,
+	historiaLaboral: [HistoriaLaboralSchema],
+	activo: Boolean,
 });
 
-AgenteSchema.methods.nombreCompleto = function(cb) {
-    const apellido = this.apellido || '';
-    const nombre = this.nombre || '';
-    return `${apellido}, ${nombre}`
-  };
+AgenteSchema.methods.nombreCompleto = function (cb) {
+	const apellido = this.apellido || "";
+	const nombre = this.nombre || "";
+	return `${apellido}, ${nombre}`;
+};
 
-AgenteSchema.methods.servicios = function(cb) {
-    if (this.activo && this.situacionLaboral && this.situacionLaboral.cargo){
-      return this.situacionLaboral.cargo.jefeServicios;
-    }
-    else {
-      return [];
-    }
-}
+AgenteSchema.methods.servicios = function (cb) {
+	if (this.activo && this.situacionLaboral && this.situacionLaboral.cargo) {
+		return this.situacionLaboral.cargo.jefeUbicaciones;
+	} else {
+		return [];
+	}
+};
 
 /**
  * Indice para la busquedas de texto libre en los campos definidos
  */
-AgenteSchema.index({
-    documento: 'text',
-    cuil: 'text',
-    nombre: 'text',
-    apellido: 'text',
-    numero: 'text'
-  }, {
-    weights: {
-      numero: 6,
-      cuil: 5,
-      documento: 4,
-      nombre: 3,
-      apellido: 2
-    },
-  });
+AgenteSchema.index(
+	{
+		documento: "text",
+		cuil: "text",
+		nombre: "text",
+		apellido: "text",
+		numero: "text",
+	},
+	{
+		weights: {
+			numero: 6,
+			cuil: 5,
+			documento: 4,
+			nombre: 3,
+			apellido: 2,
+		},
+	}
+);
 
 // AgenteSchema.plugin(audit.plugin);
 AgenteSchema.plugin(audit.plugin, { omit: ["_id"] });
 
-export const Agente = model('Agente', AgenteSchema, 'agentes');
+export const Agente = model("Agente", AgenteSchema, "agentes");
