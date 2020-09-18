7fb5cbb0639b178d800241375ca62abf6e4803cd
David Nievas <nievas.david@gmail.com>
2020-09-18 16:09:10 -0300

    Permisos. Actualizacion nombre de permisos

--- auth/permisos.ts
+++ auth/permisos.ts
@@ -3,90 +3,149 @@ export default [
 	{
 		key: "agentes",
 		title: "Módulo de Agentes",
-		levels: 1,
 		childs: [
 			{
-				key: "view_agente",
-				title: "Ver/Listar Agentes",
-				type: "boolean",
+				key: "agente",
+				title: "Agentes",
+				childs: [
+					{
+						key: "add_agente",
+						title: "Alta de Agentes",
+						type: "boolean",
+					},
+					{
+						key: "view_agente",
+						title: "Ver/Listar Agentes",
+						type: "boolean",
+					},
+					{
+						key: "change_agente",
+						title: "Modificar un Agente",
+						comment:
+							"Permite modificar los datos generales de un agente",
+						type: "boolean",
+					},
+					{
+						key: "baja_agente",
+						title: "Baja de Agente",
+						comment: "Permite dar de baja un agente",
+						type: "boolean",
+					},
+					{
+						key: "reactivar_agente",
+						title: "Reactivar Agente",
+						comment: "Permite un agente dado de Baja",
+						type: "boolean",
+					},
+					{
+						key: "habilitar_fichado",
+						title: "Habilitar Fichado",
+						comment: "Habilita a un agente a fichar",
+						type: "boolean",
+					},
+					{
+						key: "print_credencial",
+						title: "Imprimir Credencial",
+						comment:
+							"Permite imprimir las credenciales de un agente",
+						type: "boolean",
+					},
+				],
 			},
 			{
-				key: "add_agente",
-				title: "Alta de Agentes",
-				type: "boolean",
+				key: "historia",
+				title: "Historia Laboral",
+				childs: [
+					{
+						key: "add_historialaboral",
+						title: "Agregar Historia Laboral",
+						comment:
+							"Permite agregar una nueva historia laboral a un agente",
+						type: "boolean",
+					},
+					{
+						key: "view_historialaboral",
+						title: "Ver Historia Laboral",
+						comment: "Permite ver la historia laboral a un agente",
+						type: "boolean",
+					},
+					{
+						key: "change_historialaboral",
+						title: "Editar Historia Laboral",
+						comment:
+							"Permite modificar la historia laboral de los agentes",
+						type: "boolean",
+					},
+					{
+						key: "delete_historialaboral",
+						title: "Eliminar Historia Laboral",
+						comment:
+							"Permite eliminar la historia laboral de los agentes",
+						type: "boolean",
+					},
+				],
 			},
 			{
-				key: "change_agente",
-				title: "Modificar un Agente",
-				comment: "Permite modificar los datos generales de un agente",
-				type: "boolean",
+				key: "nota",
+				title: "Notas",
+				childs: [
+					{
+						key: "add_nota",
+						title: "Agregar Notas",
+						comment: "Permite asociar notas a un agente",
+						type: "boolean",
+					},
+					{
+						key: "view_nota",
+						title: "Ver Notas",
+						comment: "Permite ver las notas a un agente",
+						type: "boolean",
+					},
+					{
+						key: "change_nota",
+						title: "Modificar Notas",
+						comment:
+							"Permite modificar las notas asociadas a un agente",
+						type: "boolean",
+					},
+					{
+						key: "delete_nota",
+						title: "Eliminar Notas",
+						comment: "Permite las notas asociadas a un agente",
+						type: "boolean",
+					},
+				],
 			},
 			{
-				key: "view_ausentismo",
-				title: "Ver Ausentismo",
-				comment: "Permite visualizar el ausentismo de los agentes",
-				type: "boolean",
-			},
-			{
-				key: "baja_agente",
-				title: "Baja de Agente",
-				comment: "Permite dar de baja un agente",
-				type: "boolean",
-			},
-			{
-				key: "reactivar_agente",
-				title: "Reactivar Agente",
-				comment: "Permite un agente dado de Baja",
-				type: "boolean",
-			},
-			{
-				key: "habilitar_fichado",
-				title: "Habilitar Fichado",
-				comment: "Habilita a un agente a fichar",
-				type: "boolean",
-			},
-			{
-				key: "print_credencial",
-				title: "Imprimir Credencial",
-				comment: "Permite imprimir las credenciales de un agente",
-				type: "boolean",
-			},
-			{
-				key: "add_historialaboral",
-				title: "Agregar Historia Laboral",
-				comment:
-					"Permite agregar una nueva historia laboral a un agente",
-				type: "boolean",
-			},
-			{
-				key: "change_historialaboral",
-				title: "Editar Historia Laboral",
-				comment: "Permite modificar la historia laboral de los agentes",
-				type: "boolean",
-			},
-			{
-				key: "delete_historialaboral",
-				title: "Eliminar Historia Laboral",
-				comment: "Permite eliminar la historia laboral de los agentes",
-				type: "boolean",
-			},
-			{
-				key: "add_nota",
-				title: "Agregar Notas",
-				comment: "Permite asociar notas a un agente",
-				type: "boolean",
-			},
-			{
-				key: "change_nota",
-				title: "Modificar Notas",
-				comment: "Permite modificar las notas asociadas a un agente",
-				type: "boolean",
-			},
-			{
-				key: "delete_nota",
-				title: "Eliminar Notas",
-				comment: "Permite las notas asociadas a un agente",
-				type: "boolean",
+				key: "ausentismo",
+				title: "Ausentismo",
+				childs: [
+					{
+						key: "add_ausentismo",
+						title: "Agregar Ausencias",
+						comment: "Permite cargar ausencias a un agente",
+						type: "boolean",
+					},
+					{
+						key: "change_ausentismo",
+						title: "Modificar Ausencias",
+						comment: "Permite modificar las ausencias a un agente",
+						type: "boolean",
+					},
+					{
+						key: "view_ausentismo",
+						title: "Ver Ausentismo",
+						comment:
+							"Permite visualizar el ausentismo de los agentes",
+						type: "boolean",
+					},
+					{
+						key: "delete_ausentismo",
+						title: "Modificar Ausentismo",
+						comment: "Permite modificar las ausencias de un agente",
+						type: "boolean",
+					},
+				],
 			},
 		],
 	},
@@ -94,51 +153,62 @@ export default [
 	{
 		key: "partes",
 		title: "Módulo de Partes",
-		levels: 1,
 		childs: [
 			{
-				key: "view_parte",
-				title: "Ver/Listar Partes",
-				comment: "Permite ver o listar partes.",
-				type: "boolean",
+				key: "parte",
+				title: "Partes",
+				childs: [
+					{
+						key: "view_parte",
+						title: "Ver/Listar Partes",
+						comment: "Permite ver o listar partes.",
+						type: "boolean",
+					},
+					{
+						key: "add_parte",
+						title: "Crear Partes",
+						comment: "Permite crear partes.",
+						type: "boolean",
+					},
+					{
+						key: "change_parte",
+						title: "Editar Partes",
+						comment: "Permite editar partes.",
+						type: "boolean",
+					},
+					{
+						key: "confirmar_parte",
+						title: "Confirmar Partes",
+						comment: "Permite confirmar partes.",
+						type: "boolean",
+					},
+					{
+						key: "procesar_parte",
+						title: "Procesar un Parte",
+						comment:
+							"Permite procesar los partes elaborados por los jefes de servicio",
+						type: "boolean",
+					},
+				],
 			},
 			{
-				key: "add_parte",
-				title: "Crear Partes",
-				comment: "Permite crear partes.",
-				type: "boolean",
-			},
-			{
-				key: "change_parte",
-				title: "Editar Partes",
-				comment: "Permite editar partes.",
-				type: "boolean",
-			},
-			{
-				key: "confirmar_parte",
-				title: "Confirmar Partes",
-				comment: "Permite confirmar partes.",
-				type: "boolean",
-			},
-			{
-				key: "procesar_parte",
-				title: "Procesar un Parte",
-				comment:
-					"Permite procesar los partes elaborados por los jefes de servicio",
-				type: "boolean",
-			},
-			{
-				key: "report_fichadas",
-				title: "Reporte de Ingresos y Egresos",
-				comment:
-					"Permite generar un reporte de ingresos y egresos (fichadas)",
-				type: "boolean",
-			},
-			{
-				key: "report_partes",
-				title: "Reporte de Partes",
-				comment: "Permite generar un reporte de Partes",
-				type: "boolean",
+				key: "reporte",
+				title: "Reportes",
+				childs: [
+					{
+						key: "fichadas",
+						title: "Reporte de Ingresos y Egresos",
+						comment:
+							"Permite generar un reporte de ingresos y egresos (fichadas)",
+						type: "boolean",
+					},
+					{
+						key: "partes",
+						title: "Reporte de Partes",
+						comment: "Permite generar un reporte de Partes",
+						type: "boolean",
+					},
+				],
 			},
 		],
 	},
@@ -146,37 +216,49 @@ export default [
 	{
 		key: "reportes",
 		title: "Módulo de Reportes",
-		levels: 1,
 		childs: [
 			{
-				key: "report_legajo_agentes",
-				title: "Reporte Legajo de Agentes",
-				comment: "",
-				type: "boolean",
+				key: "agente",
+				title: "Reportes Agentes",
+				childs: [
+					{
+						key: "legajo_agentes",
+						title: "Legajo de Agentes",
+						comment: "",
+						type: "boolean",
+					},
+
+					{
+						key: "listado_agentes",
+						title: "Listado de Agentes",
+						comment: "",
+						type: "boolean",
+					},
+				],
 			},
 			{
-				key: "report_listado_agentes",
-				title: "Reporte Listado de Agentes",
-				comment: "",
-				type: "boolean",
-			},
-			{
-				key: "report_ausentismo",
-				title: "Reporte de Ausentismo",
-				comment: "",
-				type: "boolean",
-			},
-			{
-				key: "report_ausentismo_totales",
-				title: "Reporte de Ausentimos. Totales por Articulo",
-				comment: "",
-				type: "boolean",
-			},
-			{
-				key: "report_licencias",
-				title: "Reporte de Licencias",
-				comment: "",
-				type: "boolean",
+				key: "ausentismo",
+				title: "Reportes Ausentismo",
+				childs: [
+					{
+						key: "ausentismo",
+						title: "Ausentismo",
+						comment: "",
+						type: "boolean",
+					},
+					{
+						key: "ausentismo_totales",
+						title: "Ausentimos. Totales por Articulo",
+						comment: "",
+						type: "boolean",
+					},
+					{
+						key: "licencias",
+						title: "Licencias",
+						comment: "",
+						type: "boolean",
+					},
+				],
 			},
 		],
 	},
@@ -184,32 +266,31 @@ export default [
 	{
 		key: "config",
 		title: "Módulo de Configuración",
-		levels: 2,
 		childs: [
 			{
-				key: "config-articulos",
-				title: "Configuración Articulos",
+				key: "feriado",
+				title: "Feriados",
 				childs: [
 					{
-						key: "view_articulo",
+						key: "view_feriado",
 						title: "Ver Feriado",
 						comment: "Ver detalle de un Feriado",
 						type: "boolean",
 					},
 					{
-						key: "change_articulo",
+						key: "change_feriado",
 						title: "Modificar Feriado",
 						comment: "Modificar un Feriado",
 						type: "boolean",
 					},
 					{
-						key: "add_articulo",
+						key: "add_feriado",
 						title: "Alta de Feriado",
 						comment: "Alta de Feriado",
 						type: "boolean",
 					},
 					{
-						key: "delete_articulo",
+						key: "delete_feriado",
 						title: "Eliminar Feriado",
 						comment: "Eliminar un Feriado",
 						type: "boolean",
@@ -217,29 +298,29 @@ export default [
 				],
 			},
 			{
-				key: "config-feriados",
-				title: "Configuración Días Feriados",
+				key: "articulo",
+				title: "Articulos",
 				childs: [
 					{
-						key: "view_feriado",
+						key: "view_articulo",
 						title: "Ver Artículo",
 						comment: "Ver detalle de un Artículo",
 						type: "boolean",
 					},
 					{
-						key: "change_feriado",
+						key: "change_articulo",
 						title: "Modificar Artículo",
 						comment: "Modificar un Artículo",
 						type: "boolean",
 					},
 					{
-						key: "add_feriado",
+						key: "add_articulo",
 						title: "Alta de Artículo",
 						comment: "Alta de Artículo",
 						type: "boolean",
 					},
 					{
-						key: "delete_feriado",
+						key: "delete_articulo",
 						title: "Eliminar Artículo",
 						comment: "Eliminar un Artículo",
 						type: "boolean",
@@ -247,8 +328,8 @@ export default [
 				],
 			},
 			{
-				key: "config-licencias",
-				title: "Configuración Licencias por Año",
+				key: "licencia",
+				title: "Licencias por Año",
 				childs: [
 					{
 						key: "view_licencia",
@@ -277,8 +358,8 @@ export default [
 				],
 			},
 			{
-				key: "config-regimenes",
-				title: "Configuración de Regímenes",
+				key: "regimen",
+				title: "Regímenes",
 				childs: [
 					{
 						key: "view_regimen",
@@ -306,49 +387,108 @@ export default [
 					},
 				],
 			},
+			{
+				key: "guardia_lote",
+				title: "Guardia Lotes",
+				childs: [
+					{
+						key: "add_lote",
+						title: "Alta de un Lote",
+						comment: "Alta de Lote",
+						type: "boolean",
+					},
+					{
+						key: "view_lote",
+						title: "Ver Lote",
+						comment: "Ver detalle de un Lote",
+						type: "boolean",
+					},
+					{
+						key: "change_lote",
+						title: "Modificar Lote",
+						comment: "Modificar un Lote",
+						type: "boolean",
+					},
+					{
+						key: "delete_lote",
+						title: "Eliminar un Lote",
+						comment: "Eliminar un Lote",
+						type: "boolean",
+					},
+				],
+			},
+			{
+				key: "guardia_periodo",
+				title: "Guardia Lotes",
+				childs: [
+					{
+						key: "add_periodo",
+						title: "Alta de un Periodo",
+						comment: "Alta de Periodo",
+						type: "boolean",
+					},
+					{
+						key: "view_periodo",
+						title: "Ver Periodo",
+						comment: "Ver detalle de un Periodo",
+						type: "boolean",
+					},
+					{
+						key: "change_periodo",
+						title: "Modificar Periodo",
+						comment: "Modificar un Periodo",
+						type: "boolean",
+					},
+					{
+						key: "delete_periodo",
+						title: "Eliminar un Periodo",
+						comment: "Eliminar un Periodo",
+						type: "boolean",
+					},
+				],
+			},
 		],
 	},
 	// Módulo de Seguridad
 	{
 		key: "seguridad",
 		title: "Módulo de Seguridad",
-		levels: 2,
 		childs: [
 			{
-				key: "seguridad-roles",
-				title: "Roles",
+				key: "usuario",
+				title: "Usuarios",
 				childs: [
 					{
-						key: "seguridad-usuarios",
-						title: "Usuarios",
-						childs: [
-							{
-								key: "view_usuario",
-								title: "Ver Usuario",
-								comment: "Ver detalle de un Usuario",
-								type: "boolean",
-							},
-							{
-								key: "change_usuario",
-								title: "Modificar Usuario",
-								comment:
-									"Permite modificar los permisos/roles a un usuario",
-								type: "boolean",
-							},
-							{
-								key: "add_usuario",
-								title: "Alta de Usuario",
-								comment: "Permite crear nuevos usuarios",
-								type: "boolean",
-							},
-							{
-								key: "delete_usuario",
-								title: "Eliminar Usuario",
-								comment: "Eliminar usuarios",
-								type: "boolean",
-							},
-						],
+						key: "view_usuario",
+						title: "Ver Usuario",
+						comment: "Ver detalle de un Usuario",
+						type: "boolean",
 					},
+					{
+						key: "change_usuario",
+						title: "Modificar Usuario",
+						comment:
+							"Permite modificar los permisos/roles a un usuario",
+						type: "boolean",
+					},
+					{
+						key: "add_usuario",
+						title: "Alta de Usuario",
+						comment: "Permite crear nuevos usuarios",
+						type: "boolean",
+					},
+					{
+						key: "delete_usuario",
+						title: "Eliminar Usuario",
+						comment: "Eliminar usuarios",
+						type: "boolean",
+					},
+				],
+			},
+			{
+				key: "rol",
+				title: "Roles",
+				childs: [
 					{
 						key: "view_rol",
 						title: "Ver Rol",
