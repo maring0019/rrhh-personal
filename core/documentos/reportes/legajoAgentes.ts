1c102e41a9980c233e77fe699e64bbb97e1f4fa8
David Nievas <nievas.david@gmail.com>
2020-09-21 14:23:41 -0300

    Reportes.Legajo Agentes. Fix parsing 'fecha' en NormaLegal. Change 'ubicacion' por 'servicio' en cargo agente

--- core/documentos/reportes/legajoAgentes.ts
+++ core/documentos/reportes/legajoAgentes.ts
@@ -1,46 +1,46 @@
 import { Types } from "mongoose";
-import * as aqp from 'api-query-params';
+import * as aqp from "api-query-params";
 
 import { DocumentoPDF } from "../documentos";
 import { Agente } from "../../../modules/agentes/schemas/agente";
 
-import config from '../../../confg';
+import config from "../../../confg";
+import * as utils from "../utils";
 
 // TODO Falta implementar el tema de las bajas!!!
 // Faltan crear los indices por defecto
 
 export class DocumentoLegajoAgente extends DocumentoPDF {
-    templateName = 'reportes/agentes-legajo.ejs';
-    outputFilename = `${config.app.uploadFilesPath}/legajo.pdf`;
+	templateName = "reportes/agentes-legajo.ejs";
+	outputFilename = `${config.app.uploadFilesPath}/legajo.pdf`;
 
-    generarCSS() {
-        return '';
-    }
-    
-    async getContextData(){
-        let agentes = [];
-        try{
-            // Este reporte no tiene opciones de agrupamiento
-            let query = aqp(this.request.query, {
-                casters: {
-                    documentoId: val => Types.ObjectId(val),
-                },
-                castParams: {
-                    '_id': 'documentoId',
-                    'situacionLaboral.cargo.sector._id': 'documentoId'
-                }
-            })
-            // Search Pipeline
-            let pipeline:any = [
-                { $match: query.filter || {}} ,
-                { $sort: query.sort || { apellido: 1 }}
-            ]
-            agentes = await Agente.aggregate(pipeline);
-            return { agentes: agentes }
-        }
-        catch{
-            return { agentes: agentes }
-        }
-        
-    }
+	generarCSS() {
+		return "";
+	}
+
+	async getContextData() {
+		let agentes = [];
+		try {
+			// Este reporte no tiene opciones de agrupamiento
+			let query = aqp(this.request.query, {
+				casters: {
+					documentoId: (val) => Types.ObjectId(val),
+				},
+				castParams: {
+					_id: "documentoId",
+					"situacionLaboral.cargo.sector._id": "documentoId",
+				},
+			});
+			// Search Pipeline
+            let filters = utils.cleanFilters(query.filter);
+			let pipeline: any = [
+				{ $match: filters || {} },
+				{ $sort: query.sort || { apellido: 1 } },
+			];
+			agentes = await Agente.aggregate(pipeline);
+			return { agentes: agentes };
+		} catch {
+			return { agentes: agentes };
+		}
+	}
 }
--- core/documentos/utils.ts
+++ core/documentos/utils.ts
@@ -1,50 +1,57 @@
 export function title(value: any, arg1?: string): any {
-    if(!value){
-        return '---';
-    }
-    else{
-        if (typeof value === 'string' || typeof value === 'number'){
-            return titleCase(''+value);
-        }
-        else{
-            if (arg1 && arg1 in value){
-                return title(value[arg1]);
-            }
-            else{
-                return titleCase(''+value);
-            }
-        }
-    }
+	if (!value) {
+		return "---";
+	} else {
+		if (typeof value === "string" || typeof value === "number") {
+			return titleCase("" + value);
+		} else {
+			if (arg1 && arg1 in value) {
+				return title(value[arg1]);
+			} else {
+				return titleCase("" + value);
+			}
+		}
+	}
 }
 
 export function titleCase(str) {
-    const title =str.toLowerCase().split(' ').map(function (word) {
-        return (word.charAt(0).toUpperCase() + word.slice(1));
-    }).join(' ');
-    return title;
+	const title = str
+		.toLowerCase()
+		.split(" ")
+		.map(function (word) {
+			return word.charAt(0).toUpperCase() + word.slice(1);
+		})
+		.join(" ");
+	return title;
 }
 
-
-export function getQueryParam(filter, filterCondition ){
-    let filterField;
-    if (filter && filter[filterCondition]){
-        filterField = filter[filterCondition];
-    }
-    return filterField;
+export function getQueryParam(filter, filterCondition) {
+	let filterField;
+	if (filter && filter[filterCondition]) {
+		filterField = filter[filterCondition];
+	}
+	return filterField;
 }
 
-export function cleanFilters(filter){
-    let blacklist = ['$group', 'fechaDesde', 'fechaHasta', 'articulos', 'anios'];
-    Object.keys(filter).forEach(field => {
-        if (blacklist.indexOf(field)>=0) delete filter[field];        
-    });
-    return filter;
+export function cleanFilters(filter) {
+	let blacklist = [
+		"$group",
+		"fechaDesde",
+		"fechaHasta",
+		"articulos",
+		"anios",
+		"token",
+	];
+	Object.keys(filter).forEach((field) => {
+		if (blacklist.indexOf(field) >= 0) delete filter[field];
+	});
+	return filter;
 }
 
-export function projectionToArray(extraFields){
-    let output = [];
-    Object.keys(extraFields).forEach(field => {
-        output.push(field);
-    });
-    return output;
+export function projectionToArray(extraFields) {
+	let output = [];
+	Object.keys(extraFields).forEach((field) => {
+		output.push(field);
+	});
+	return output;
 }
