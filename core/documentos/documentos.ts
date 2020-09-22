aa1638dbc86d5e1a274ae9f3eee66cd6c51305fc
David Nievas <nievas.david@gmail.com>
2020-09-22 12:28:48 -0300

    Prettier format fixs

--- core/documentos/documentos.ts
+++ core/documentos/documentos.ts
@@ -1,149 +1,151 @@
-import * as fs from 'fs';
-import * as pdf from 'html-pdf';
-import * as scss from 'node-sass';
-import * as path from 'path';
+import * as fs from "fs";
+import * as pdf from "html-pdf";
+import * as scss from "node-sass";
+import * as path from "path";
 
-import * as ejs from 'ejs';
+import * as ejs from "ejs";
 
-import config from '../../confg';
+import config from "../../confg";
 
 /**
- * Base Class para generar dpcumentos PDF. 
+ * Base Class para generar dpcumentos PDF.
  */
 export class DocumentoPDF {
+	templateName: any;
+	template: any;
+	html: any = "";
+	ctx: any = {};
+	outputFilename = "documento.pdf";
+	headerLogo = `${config.app.url}:${config.app.port}/static/images/logo_hospital.jpeg`;
 
-    templateName:any;
-    template:any;
-    html:any = '';
-    ctx:any = {};
-    outputFilename = 'documento.pdf';
-    headerLogo = `${config.app.url}:${config.app.port}/static/images/logo_hospital.jpeg`;
-    
-    request:any;
+	request: any;
 
-    private static options: pdf.CreateOptions = {};
+	private static options: pdf.CreateOptions = {};
 
-    protected getTemplateName(){
-        if (this.templateName){
-            return this.templateName;
-        }
-        else{
-            throw new Error('No se indico ningun path para leer el template');
-        }
-    }
+	protected getTemplateName() {
+		if (this.templateName) {
+			return this.templateName;
+		} else {
+			throw new Error("No se indico ningun path para leer el template");
+		}
+	}
 
-    protected getTemplate(){
-        if (this.template) {
-            return this.template;
-        }
-        else{
-            // Try to read template from filesystem
-            if (this.getTemplateName()){
-                try {
-                    let template = fs.readFileSync(path.join(config.app.templateRootPath, this.getTemplateName()), 'utf8');
-                    return template;
-                }
-                catch(fileError){
-                    throw new Error('No se pudo leer el path indicado');
-                }
-            }
-            else{
-                throw new Error('No HTML Template to read');
-            }
-        }   
-    }
+	protected getTemplate() {
+		if (this.template) {
+			return this.template;
+		} else {
+			// Try to read template from filesystem
+			if (this.getTemplateName()) {
+				try {
+					let template = fs.readFileSync(
+						path.join(
+							config.app.templateRootPath,
+							this.getTemplateName()
+						),
+						"utf8"
+					);
+					return template;
+				} catch (fileError) {
+					throw new Error("No se pudo leer el path indicado");
+				}
+			} else {
+				throw new Error("No HTML Template to read");
+			}
+		}
+	}
 
-    private getFilePath(){
-        return path.join(config.app.templateRootPath, this.getTemplateName());
-    }
+	private getFilePath() {
+		return path.join(config.app.templateRootPath, this.getTemplateName());
+	}
 
-    
-    protected async getContextData(){
-        return this.ctx;
-    }
+	protected async getContextData() {
+		return this.ctx;
+	}
 
-    protected getOutputFilename(){
-        return this.outputFilename;
-    }
+	protected getOutputFilename() {
+		return this.outputFilename;
+	}
 
-    protected getDocumentoOptions(){
-        let phantomPDFOptions: pdf.CreateOptions = {
-            format: 'A4',
-            border: {
-                // default is 0, units: mm, cm, in, px
-                top: '.25cm',
-                right: '0cm',
-                bottom: '3cm',
-                left: '0cm'
-            },
-            header: {
-                height: '5.75cm',
-            },
-            footer: {
-                height: '1cm',
-                contents: {}
-            }
-        };
+	protected getDocumentoOptions() {
+		let phantomPDFOptions: pdf.CreateOptions = {
+			format: "A4",
+			border: {
+				// default is 0, units: mm, cm, in, px
+				top: ".25cm",
+				right: "0cm",
+				bottom: "3cm",
+				left: "0cm",
+			},
+			header: {
+				height: "5.75cm",
+			},
+			footer: {
+				height: "1cm",
+				contents: {},
+			},
+		};
 
-        DocumentoPDF.options = DocumentoPDF.options || phantomPDFOptions;
-        let options = DocumentoPDF.options || phantomPDFOptions;
-        return options;
-    }
+		DocumentoPDF.options = DocumentoPDF.options || phantomPDFOptions;
+		let options = DocumentoPDF.options || phantomPDFOptions;
+		return options;
+	}
 
-    protected generarCSS() {
-        let css = '<style>\n\n';
-        let files = ['css/style.scss']
-        for (const file of files) {
-            let scssFile = path.join(config.app.publicFolder, file);
-            css += scss.renderSync({ // SCSS => CSS
-                file: scssFile
-            }).css;
-        }
-        css += '</style>';
-        return css;
-    }
+	protected generarCSS() {
+		let css = "<style>\n\n";
+		let files = ["css/style.scss"];
+		for (const file of files) {
+			let scssFile = path.join(config.app.publicFolder, file);
+			css += scss.renderSync({
+				// SCSS => CSS
+				file: scssFile,
+			}).css;
+		}
+		css += "</style>";
+		return css;
+	}
 
-    protected parseHTML(htmlTemplate, ctx){
-        let htmlStr = this.html? this.html : htmlTemplate ;
-        return ejs.render(htmlStr, ctx, { filename: this.getFilePath()});
-    }
+	protected parseHTML(htmlTemplate, ctx) {
+		let htmlStr = this.html ? this.html : htmlTemplate;
+		return ejs.render(htmlStr, ctx, { filename: this.getFilePath() });
+	}
 
-    async generarHTML() {
-        let htmlTemplate = this.getTemplate();
-        let ctx = await this.getContextData();
-        let html = this.parseHTML(htmlTemplate, ctx);
-        html = html + this.generarCSS();
-        return html    
-    }
+	async generarHTML() {
+		let htmlTemplate = this.getTemplate();
+		let ctx = await this.getContextData();
+		let html = this.parseHTML(htmlTemplate, ctx);
+		html = html + this.generarCSS();
+		return html;
+	}
 
-    async generarPDFFile(html){
-        return new Promise((resolve, reject) => {
-            let options = this.getDocumentoOptions();
-            pdf.create(html, options).toFile(this.getOutputFilename(), (err, file): any => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(file.filename);
-            });
-        });
-    }
+	async generarPDFFile(html) {
+		return new Promise((resolve, reject) => {
+			let options = this.getDocumentoOptions();
+			pdf.create(html, options).toFile(
+				this.getOutputFilename(),
+				(err, file): any => {
+					if (err) {
+						reject(err);
+					}
+					resolve(file.filename);
+				}
+			);
+		});
+	}
 
-    async getPDF(req) {
-        try {
-            this.request  = req;
-            let html = await this.generarHTML();
-            let file = await this.generarPDFFile(html);
-            return file;
-        }
-        catch(err){
-            return err;
-        }
-    }
+	async getPDF(req) {
+		try {
+			this.request = req;
+			let html = await this.generarHTML();
+			let file = await this.generarPDFFile(html);
+			return file;
+		} catch (err) {
+			return err;
+		}
+	}
 
-    async getHTML(req) {
-            this.request  = req;
-            let html = await this.generarHTML();
-            return html;
-    }
-
+	async getHTML(req) {
+		this.request = req;
+		let html = await this.generarHTML();
+		return html;
+	}
 }
--- core/documentos/reportes/licenciasPorAgente.ts
+++ core/documentos/reportes/licenciasPorAgente.ts
@@ -1,98 +1,100 @@
 import { Types } from "mongoose";
-import * as aqp from 'api-query-params';
+import * as aqp from "api-query-params";
 
 import { DocumentoPDF } from "../documentos";
 import { Agente } from "../../../modules/agentes/schemas/agente";
 
 import * as utils from "../utils";
-import config from '../../../confg';
+import config from "../../../confg";
 
 export class DocumentoLicenciasPorAgente extends DocumentoPDF {
-    templateName = 'reportes/agentes-licencias.ejs';
-    outputFilename = `${config.app.uploadFilesPath}/licenciasporarticulo.pdf`;
+	templateName = "reportes/agentes-licencias.ejs";
+	outputFilename = `${config.app.uploadFilesPath}/licenciasporarticulo.pdf`;
 
-    generarCSS() {
-        return '';
-    }
-    
-    async getContextData(){
-        // Recuperamos todas las opciones para el reporte (filtros, orden, etc)
-        let query = aqp(this.request.query, {
-            casters: {
-                documentoId: val => Types.ObjectId(val),
-              },
-              castParams: {
-                '_id': 'documentoId',
-                'situacionLaboral.cargo.sector._id': 'documentoId',
-                'articulos': 'documentoId'
-              }
-        })
-        // Identificamos el campo por el cual agrupar. Si no se especifico agregamos
-        // uno por defecto
-        let groupField = utils.getQueryParam(query.filter, '$group');
-        if (!groupField) groupField = 'situacionLaboral.cargo.sector.nombre';
-        const groupCondition = { _id : `$${groupField}`, agentes: { $push: "$$ROOT" } }
-        
-        // Filtros para las licencias
-        let anios = utils.getQueryParam(query.filter, 'anios');
-        if (anios) {
-            anios = anios.$in? anios.$in: [anios];
-        }
-        else{
-            anios = [];// TODO Raise error (Agregar todas las validaciones necesaras)
-        }
-        // Preparamos las opciones de filtrado sobre el agente. Removemos filtros no requeridos
-        let filterCondition = utils.cleanFilters(query.filter);
-        
-        // Aggregation Framework Pipeline
-        let pipeline:any = [
-            { $match: filterCondition || {}} ,
-            { $sort: query.sort || { apellido: 1 }},
-            { $lookup: {
-                    from: "indicadoresAusentismo",
-                    let: { agente_id: "$_id"},
-                    pipeline: 
-                        [{ 
-                            $match: { 
-                                $expr: {
-                                    $and: [ 
-                                        { $eq: ["$$agente_id", "$agente._id"]}, // Join con agente id
-                                        { $in: ["$vigencia", anios ]},
-                                        ]
-                                    },
-                                }
-                            },
-                            { $sort: { vigencia: 1 }}
-                        ],
-                    as: "ausentismo"
-                 }
-            } ,
-            { $group: groupCondition},
-            { $sort: { _id:1 }}
-        ]
+	generarCSS() {
+		return "";
+	}
 
-        let gruposAgentes = await Agente.aggregate(pipeline);
-        
-        return { 
-                gruposAgente: gruposAgentes,
-                anios: anios
-            }
-    }
+	async getContextData() {
+		// Recuperamos todas las opciones para el reporte (filtros, orden, etc)
+		let query = aqp(this.request.query, {
+			casters: {
+				documentoId: (val) => Types.ObjectId(val),
+			},
+			castParams: {
+				_id: "documentoId",
+				"situacionLaboral.cargo.sector._id": "documentoId",
+				articulos: "documentoId",
+			},
+		});
+		// Identificamos el campo por el cual agrupar. Si no se especifico agregamos
+		// uno por defecto
+		let groupField = utils.getQueryParam(query.filter, "$group");
+		if (!groupField) groupField = "situacionLaboral.cargo.sector.nombre";
+		const groupCondition = {
+			_id: `$${groupField}`,
+			agentes: { $push: "$$ROOT" },
+		};
 
+		// Filtros para las licencias
+		let anios = utils.getQueryParam(query.filter, "anios");
+		if (anios) {
+			anios = anios.$in ? anios.$in : [anios];
+		} else {
+			anios = []; // TODO Raise error (Agregar todas las validaciones necesaras)
+		}
+		// Preparamos las opciones de filtrado sobre el agente. Removemos filtros no requeridos
+		let filterCondition = utils.cleanFilters(query.filter);
 
-    getFilterField(filter, filterCondition ){
-        let filterField;
-        if (filter && filter[filterCondition]){
-            filterField = filter[filterCondition];
-        }
-        return filterField;
-    }
+		// Aggregation Framework Pipeline
+		let pipeline: any = [
+			{ $match: filterCondition || {} },
+			{ $sort: query.sort || { apellido: 1 } },
+			{
+				$lookup: {
+					from: "indicadoresAusentismo",
+					let: { agente_id: "$_id" },
+					pipeline: [
+						{
+							$match: {
+								$expr: {
+									$and: [
+										{ $eq: ["$$agente_id", "$agente._id"] }, // Join con agente id
+										{ $in: ["$vigencia", anios] },
+									],
+								},
+							},
+						},
+						{ $sort: { vigencia: 1 } },
+					],
+					as: "ausentismo",
+				},
+			},
+			{ $group: groupCondition },
+			{ $sort: { _id: 1 } },
+		];
 
-    projectionToArray(extraFields){
-        let output = [];
-        Object.keys(extraFields).forEach(field => {
-            output.push(field);
-        });
-        return output;
-    }
+		let gruposAgentes = await Agente.aggregate(pipeline);
+
+		return {
+			gruposAgente: gruposAgentes,
+			anios: anios,
+		};
+	}
+
+	getFilterField(filter, filterCondition) {
+		let filterField;
+		if (filter && filter[filterCondition]) {
+			filterField = filter[filterCondition];
+		}
+		return filterField;
+	}
+
+	projectionToArray(extraFields) {
+		let output = [];
+		Object.keys(extraFields).forEach((field) => {
+			output.push(field);
+		});
+		return output;
+	}
 }
