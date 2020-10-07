da5109eb8e7636ccd66308b115a2e152a25c51f4
David Nievas <nievas.david@gmail.com>
2020-10-07 17:28:34 -0300

    Reportes. Ausencias por Agentes. Fix calculo de dias de licencias

--- core/documentos/reportes/ausenciasPorAgente.ts
+++ core/documentos/reportes/ausenciasPorAgente.ts
@@ -7,6 +7,7 @@ import { Agente } from "../../../modules/agentes/schemas/agente";
 import * as utils from "../utils";
 import config from '../../../confg';
 
+
 export class DocumentoAusenciasPorAgente extends DocumentoPDF {
     templateName = 'reportes/agentes-ausencias.ejs';
     outputFilename = `${config.app.uploadFilesPath}/ausenciasporagente.pdf`;
@@ -17,16 +18,7 @@ export class DocumentoAusenciasPorAgente extends DocumentoPDF {
     
     async getContextData(){
         // Recuperamos todas las opciones para el reporte (filtros, orden, etc)
-        let query = aqp(this.request.query, {
-            casters: {
-                documentoId: val => Types.ObjectId(val),
-              },
-              castParams: {
-                '_id': 'documentoId',
-                'situacionLaboral.cargo.sector._id': 'documentoId',
-                'articulos': 'documentoId'
-              }
-        })
+        let query = this.getQueryOptions();
         // Identificamos el campo por el cual agrupar. Si no se especifico agregamos
         // uno por defecto
         let groupField = utils.getQueryParam(query.filter, '$group');
@@ -37,11 +29,10 @@ export class DocumentoAusenciasPorAgente extends DocumentoPDF {
         let fechaDesde = utils.getQueryParam(query.filter, 'fechaDesde'); // Format 2016-01-01
         let fechaHasta = utils.getQueryParam(query.filter, 'fechaHasta');
         let articulosIds = utils.getQueryParam(query.filter, 'articulos');
+        let filterArticulos: any = { $ne : null };
         if (articulosIds) {
             articulosIds = articulosIds.$in? articulosIds.$in: [articulosIds];
-        }
-        else{
-            articulosIds = [];
+            filterArticulos = { $in : articulosIds }
         }
         // Preparamos las opciones de filtrado sobre el agente. Removemos filtros no requeridos
         let filterCondition = utils.cleanFilters(query.filter);
@@ -51,32 +42,61 @@ export class DocumentoAusenciasPorAgente extends DocumentoPDF {
             { $match: filterCondition || {}} ,
             { $sort: query.sort || { apellido: 1 }},
             { $lookup: {
-                    from: "ausenciasperiodo",
-                    let: { agente_id: "$_id", fecha_desde: fechaDesde, fecha_hasta: fechaHasta},
-                    pipeline: 
-                        [{ 
-                            $match: { 
-                                $expr: {
-                                    $and: [ 
-                                        { $eq: ["$$agente_id", "$agente._id"]}, // Join con agente id
-                                        (articulosIds.length)?{ $in: ["$articulo._id", articulosIds ]}:{},
-                                        { $gte: ["$fechaDesde", "$$fecha_desde"]},
-                                        { $lte: ["$fechaHasta", "$$fecha_hasta"]}
-                                        ]
-                                    },
+                from: "ausenciasperiodo",
+                let: { agente_id: "$_id" },
+                pipeline: 
+                    [  { $match:{ 
+                            "$expr" : { $eq : ["$agente._id", "$$agente_id" ] }, // 'Join' con agentes
+                            "fechaHasta": { $gte: fechaDesde },
+                            "fechaDesde": { $lte: fechaHasta },
+                            "articulo._id": filterArticulos
+                            } 
+                        },
+                        { $unwind : "$ausencias"},
+                        { $match: { 
+                             "ausencias.fecha": { 
+                                 $gte: fechaDesde, 
+                                 $lte: fechaHasta
                                 }
                             }
-                        ],
-                    as: "ausentismo"
-                 }
-            } ,
+                        },
+                        { $group: {
+                            "_id" : { "ausentismo": "$_id", "agente":"$agente._id", "articulo":"$articulo.codigo" },
+                            "totalAusencias" : { $sum: 1 },
+                            "fechaInicio": { $min: "$ausencias.fecha" },
+                            "fechaFin": { $max: "$ausencias.fecha" }
+                            }
+                        },
+                    ],
+                as: "ausentismo"
+                }
+            },
             { $group: groupCondition},
             { $sort: { _id:1 }}
         ]
-
+    
         let gruposAgentes = await Agente.aggregate(pipeline);
+    
         return { 
-                gruposAgente: gruposAgentes
+            gruposAgente: gruposAgentes,
+            srcImgLogo: this.headerLogo
             }
+    }
+
+    /**
+	 * Recupera todo los query parameters del request.
+	 * Filtros, orden, campos a mostrar, etc
+	 */
+	getQueryOptions() {
+        return aqp(this.request.query, {
+            casters: {
+                documentoId: val => Types.ObjectId(val),
+              },
+              castParams: {
+                '_id': 'documentoId',
+                'situacionLaboral.cargo.sector._id': 'documentoId',
+                'articulos': 'documentoId'
+              }
+        })
     }
 }
