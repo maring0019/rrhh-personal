a57f6a945961d1bd08818a7b1a206283ce5d3b5b
David Nievas <nievas.david@gmail.com>
2021-02-03 10:23:04 -0300

    Seguridad. Fix issue. Al actualizar un rol el cambio impacta tambien en los usuarios que posean el rol, actualizando los permisos correspondientes.

--- modules/seguridad/controller/rol.ts
+++ modules/seguridad/controller/rol.ts
@@ -1,5 +1,54 @@
 import BaseController from "../../../core/app/basecontroller";
+import { Usuario } from "../../../auth/schemas/Usuarios";
 
-class RolController extends BaseController {}
+class RolController extends BaseController {
+
+    async update(req, res, next) {
+		try {
+			let object: any = await this.getObject(req.params.id);
+			if (!object)
+				return res
+					.status(404)
+					.send({ message: this.getMessageNotFound() });
+            
+            
+            const rolChanges = req.body;
+            const usuariosChanges = await this.updateUsuariosPerms(object, rolChanges);
+            
+            // 1. Aplicamos los cambios sobre el rol
+            object = this.prepareObjectAudit(object, rolChanges);
+            await object.updateOne(object);
+            // 2. Aplicamos los cambios sobre los usuarios
+            for (const user of usuariosChanges) {
+                await user.updateOne(user);
+            }
+            return res.json(await this.getObject(req.params.id));
+		} catch (err) {
+			return next(err);
+		}
+    }
+    
+    /**
+     * Helper method.
+     * Actualiza los permisos de los usuarios que tienen asignado el rol
+     * identificado por 'new_rol'. Basicamente quita los viejos permisos
+     * del rol y agrega los nuevos.
+     * Retornamos una lista de usuarios con sus permisos actualizados. No
+     * se persiste ningun cambio aca.
+     * @param oldRol antiguo rol con permisos originales
+     * @param newRol nuevo rol editado con los permisos actualizados
+     */
+    async updateUsuariosPerms(oldRol, newRol ){
+        let usuariosToUpdate:any = await Usuario.find({ roles: newRol.codename });
+        for (const user of usuariosToUpdate) {
+            let permisos = [...user.permisos]; 
+            // Remove all perms attached to user identified by oldRol definition
+            permisos = permisos.filter( x => !oldRol.permisos.filter( y => y === x).length)
+            // Add new perms
+            user.permisos = permisos.concat(newRol.permisos);
+            }
+        return usuariosToUpdate;
+    }
+}
 
 export default RolController;
